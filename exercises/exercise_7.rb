9d1afc2046676456d072216deaa31499529b9328
J8051 <nathaniel.jessica@yahoo.ca>
2023-05-03 13:20:01 -0400

    exercise 7 complete

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,23 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+class Employee < ActiveRecord::Base
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :hourly_rate, numericality: { only_integer: true }
+  validates :hourly_rate, length: { in: 4..200 }
+  belongs_to :store
+  validates :store, presence: true
+end
+
+class Store < ActiveRecord::Base
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: {greater_than_or_equal_to: 0}
+end
+
+puts "Please enter a store name"
+storeName = gets.chomp
+store = Store.create(name:storeName)
+puts store.valid?
+puts store.errors.full_messages
